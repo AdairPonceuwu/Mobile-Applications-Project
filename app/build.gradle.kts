cdcfb7df0f4cdf6c1a470d3fb1eb60a406d471ce
AdairPonceuwu <adair.ponce@alumno.buap.mx>
2024-04-01 16:16:27 -0600

    Add proyect

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.google.gms.google-services")
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "com.example.mobile_applications_project"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.mobile_applications_project"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //noinspection UseTomlInstead
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    //noinspection UseTomlInstead
+    implementation("com.google.firebase:firebase-analytics")
+}
