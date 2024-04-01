3c23d24d17165ac1c1cb022f7572f2b210432ea1
AdairPonceuwu <adair.ponce@alumno.buap.mx>
2024-04-01 17:12:04 -0600

    Agregando el inicio del login

---
+++ app/src/main/java/com/example/mobile_applications_project/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.mobile_applications_project;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText mTextName;
+    EditText mTextEmail;
+    EditText mTextPassword;
+
+    Button mButtonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mTextName = findViewById(R.id.textName);
+        mTextEmail = findViewById(R.id.textEmail);
+        mTextPassword = findViewById(R.id.textPassword);
+
+        mButtonRegister = findViewById(R.id.btnRegister);
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Boton Registrar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
