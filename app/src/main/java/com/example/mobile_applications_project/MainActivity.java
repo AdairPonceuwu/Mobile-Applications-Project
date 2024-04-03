f5781399765481b95ee7d5041348011e06ea466f
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-04-03 12:44:32 -0600

    Agrega importaciones y funcionalidad de botón Jugar en MainActivity. Actualiza el diseño de activity_main.xml.

--- app/src/main/java/com/example/mobile_applications_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_applications_project/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,8 +19,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     TextView mTextFecha;
 
     Button mButtonOut;
+    Button mButtonJugar;
 
 
     @Override
@@ -61,6 +67,14 @@ public class MainActivity extends AppCompatActivity {
         mTextName=findViewById(R.id.textViewCorreo);
         mTextName=findViewById(R.id.textViewFecha);
         // TEXTVIEW final
+        mButtonJugar =findViewById(R.id.btnJugar);
+        mButtonJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Boton Jugar",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
 
         mButtonOut = findViewById(R.id.btnCerrarSesion);
@@ -73,13 +87,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //Cerramos la sesion del usuario
+    // Vemos los datos del usuario Logueado inicio
+    private void UserInfo(){
+        Query query = databaseReference.orderByChild("Email").equalTo(mUser.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    String uidUsuario = ""+ds.child("Uid").getValue();
+                    String namelUsuario = ""+ds.child("Uid").getValue();
+                    String emailUsuario = ""+ds.child("Uid").getValue();
+                    String fechaUsuario = ""+ds.child("Uid").getValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+
+    // Vemos los datos del usuario Logueado final
+
+
+
+    //Cerramos la sesion del usuario inicio
     private void CerrarSesion() {
         mAuth.signOut();
         startActivity(new Intent(MainActivity.this, RegisterActivity.class));
         Toast.makeText(MainActivity.this, "Sign Out",Toast.LENGTH_SHORT).show();
     }
-    //Cerramos la sesion del usuario
+    //Cerramos la sesion del usuario final
 
 
     //Metodo que se ejecuta cuando el juego se abre
