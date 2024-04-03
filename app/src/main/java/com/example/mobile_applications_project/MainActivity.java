85847d788f8fa21d68c770d4febe204ff24b4066
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-04-03 12:24:00 -0600

    textview

--- app/src/main/java/com/example/mobile_applications_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_applications_project/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -17,11 +18,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseUser mUser;
     FirebaseAuth mAuth;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    // Firebase Final
+    TextView mTextName;
+    TextView mTextEmail;
+    TextView mTextFecha;
 
     Button mButtonOut;
 
@@ -38,9 +49,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
+        // Firebase Inicio
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Datos de mi juego");
+        // Firebase Final
+
+        // TEXTVIEW inicio
+        mTextName=findViewById(R.id.textViewNombre);
+        mTextName=findViewById(R.id.textViewCorreo);
+        mTextName=findViewById(R.id.textViewFecha);
+        // TEXTVIEW final
+
 
         mButtonOut = findViewById(R.id.btnCerrarSesion);
 
