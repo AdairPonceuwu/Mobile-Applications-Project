fade2e7243e51b3670b5780d9c69c6a47b3770da
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-04-03 15:33:38 -0600

    Agrega funcionalidad de edición de nombre de usuario

--- app/src/main/java/com/example/mobile_applications_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_applications_project/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.mobile_applications_project;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,11 +14,14 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,6 +30,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -69,8 +76,15 @@ public class MainActivity extends AppCompatActivity {
         mTextEmail=findViewById(R.id.textViewCorreo);
         mTextFecha=findViewById(R.id.textViewFecha);
         mTextIdentificador=findViewById(R.id.textViewUid);
+
         // TEXTVIEW final
         mButtonUpdate=findViewById(R.id.btnEditar);
+        mButtonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateInfo();
+            }
+        });
         mButtonJugar =findViewById(R.id.btnJugar);
         mButtonJugar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,6 +105,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
         UserInfo();
+    }
+
+    // Aqui actualizamos los datos del usuario inicio
+    private void updateInfo() {
+        String [] opciones = {"Nombre"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(i==0){
+                    UpdateName("Nombre");
+                }
+            }
+        });
+        builder.create().show();
+    }
+    // Aqui actualizamos los datos del usuario final
+    private void UpdateName(final String key) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Editar el " + key);
+        LinearLayoutCompat linearLayoutCompat = new LinearLayoutCompat(MainActivity.this);
+        linearLayoutCompat.setOrientation(LinearLayoutCompat.VERTICAL);
+        linearLayoutCompat.setPadding(10,10,10,10);
+        final EditText editText = new EditText(MainActivity.this);
+        editText.setHint("Ingrese su nuevo " + key);
+        linearLayoutCompat.addView(editText);
+        builder.setView(linearLayoutCompat);
+        // Esto saldra si el usuario hace click en actualizar info
+        builder.setPositiveButton("Actualizar informacion", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String value = editText.getText().toString().trim();
+                HashMap<String,Object> result = new HashMap<>();
+                result.put(key,value);
+                databaseReference.child(mAuth.getUid()).updateChildren(result).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this, "El nombre se actualizó correctamente", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Ha cancelado la edicion del perfil", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
     }
 
     // Vemos los datos del usuario Logueado inicio
