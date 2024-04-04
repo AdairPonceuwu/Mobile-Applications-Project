721f5f470c5320eb95a5e27db0b34b9134e5802b
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-04-04 12:56:43 -0600

    Añadir elementos de interfaz de usuario en Nivel1_Sumas_Faciles.java

--- app/src/main/java/com/example/mobile_applications_project/Nivel1_Sumas_Faciles.java
+++ app/src/main/java/com/example/mobile_applications_project/Nivel1_Sumas_Faciles.java
@@ -1,6 +1,8 @@
 package com.example.mobile_applications_project;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Nivel1_Sumas_Faciles extends AppCompatActivity {
+
+    ImageView imagenVidas;
+    TextView nombreJugador;
+    TextView puntajeJugador;
+    ImageView imagenIzquierda;
+    ImageView imagenSigno;
+    ImageView imagenDerecha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,14 @@ public class Nivel1_Sumas_Faciles extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        imagenVidas = findViewById(R.id.imageVidas);
+        nombreJugador = findViewById(R.id.textViewUsuario);
+        puntajeJugador = findViewById(R.id.textViewPuntos);
+        imagenIzquierda = findViewById(R.id.imageViewNumeroUno);
+        imagenDerecha = findViewById(R.id.imageViewNumeroDos);
+        imagenSigno = findViewById(R.id.imageViewSigno);
+
     }
 }
